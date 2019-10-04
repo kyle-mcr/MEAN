67dd054ee1bd3b209c1614aca0dffbec6b5199ae
Kyle McReynolds <53325780+kyle-mcr@users.noreply.github.com>
2019-10-03 17:44:20 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -8,15 +8,19 @@ app.use(session({
   cookie: { maxAge: 60000 }
 }))
 app.use(express.static(__dirname + "/static"));
+app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.get('/', (req, res) => {
-    console.log("Value of name in session: ", req.session.count);
-    if(!req.session.count){
-        req.session.count = 0
-    } 
-    req.session.count += 1
-    res.render('count', {count: req.session.count});
+app.get('/', (request, response) => {
+   response.render('index', {title:"my root route"});
 });
-
+app.post("/process", (req, res) => {
+    console.log(req.body)
+    req.session.myobject = req.body
+    res.redirect('/result')
+})
+app.get("/result", (req, res) => {
+    console.log(req.body)
+    res.render('result', {myobject: req.session.myobject});
+})
  app.listen(8000, () => console.log("listening on port 8000"));
